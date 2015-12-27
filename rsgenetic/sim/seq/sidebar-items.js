0ac2e83059bc8fc6cfe13164b97e38f27b033f89
Mathieu De Coster <mth.decoster@gmail.com>
2015-12-27 10:44:44 +0100

    Update docs

--- rsgenetic/sim/seq/sidebar-items.js
+++ rsgenetic/sim/seq/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Simulator",""],["SimulatorBuilder","A `Builder` for the `Simulator` type."]]});
+initSidebarItems({"struct":[["Simulator","A sequential implementation of `::sim::Simulation`. The genetic algorithm is run in a single thread."],["SimulatorBuilder","A `Builder` for the `Simulator` type."]]});
---
+++ rsgenetic/sim/shared/select/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["Selector","A `Selector` can select `Parents` for a new iteration of a `Simulation`. For information on what the different selection functions do, refer to `SelectionType`."]]});
