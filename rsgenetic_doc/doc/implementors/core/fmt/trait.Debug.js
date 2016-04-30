7da589a4cfd8d317792b9dae0d36584168212cb8
Mathieu De Coster <mth.decoster@gmail.com>
2016-04-30 13:29:00 +0200

    Update documentation

---
+++ rsgenetic_doc/doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rsgenetic'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='rsgenetic/sim/enum.StepResult.html' title='rsgenetic::sim::StepResult'>StepResult</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='rsgenetic/sim/enum.RunResult.html' title='rsgenetic::sim::RunResult'>RunResult</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
