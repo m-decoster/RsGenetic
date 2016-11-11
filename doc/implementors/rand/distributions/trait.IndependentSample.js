c66684b748d4198ca51a59d3ae30c9c3b12eb618
Mathieu De Coster <mth.decoster@gmail.com>
2016-11-11 12:25:11 +0100

    Update docs to 1.7.0

--- doc/implementors/rand/distributions/trait.IndependentSample.js
+++ doc/implementors/rand/distributions/trait.IndependentSample.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rand"] = [];
+implementors["libc"] = [];implementors["rand"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
